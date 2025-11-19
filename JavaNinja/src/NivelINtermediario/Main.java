0957779abb106176ad5a5e52bfff398e4371c525
Watilla <wadsonwatilla@gmail.com>
2025-11-19 05:20:18 -0300

    Inicio do modulo Intermediario

--- JavaNinja/src/NivelINtermediario/Main.java
+++ JavaNinja/src/NivelINtermediario/Main.java
@@ -2,6 +2,21 @@ package NivelINtermediario;
 
 public class Main {
     public static void main(String[] args) {
+        Ninja Naruto = new Ninja();
+        Ninja Sasuke = new Ninja();
+
+
+        Naruto.nome = "Naruto";
+        Naruto.aldeia = "Aldeia da Folha";
+        Naruto.idade = 18;
+
+        Sasuke.nome = "Sasuke";
+        Sasuke.aldeia = "Aldeia da Folha";
+        Sasuke.idade = 18;
+
+
+        System.out.println(Naruto);
+        System.out.println(Sasuke);
 
     }
 }
---
+++ JavaNinja/src/NivelINtermediario/Ninja.java
@@ -0,0 +1,16 @@
+package NivelINtermediario;
+
+public class Ninja {
+    String nome;
+    int idade;
+    String aldeia;
+
+    @Override
+    public String toString() {
+        return "Ninja{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", aldeia='" + aldeia + '\'' +
+                '}';
+    }
+}
